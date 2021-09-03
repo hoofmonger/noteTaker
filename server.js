96c7d3f65492a9a5043a0142a880f675911b0776
James Hoofard <james.hoofard.iii@gmail.com>
2021-09-02 20:31:14 -0500

    moved some files to work with heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const database = require('./db/db.json');
+const database = require('./Develop/db/db.json');
 const fs = require('fs');
 const { json } = require('express');
 const {v4:uuidv4} = require('uuid')
