bcfdcba546e3052a2a0469a408cf8abee106b870
James Hoofard <james.hoofard.iii@gmail.com>
2021-09-07 08:26:40 -0500

    more deploy fixes

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+//   });
 
 
   app.get('/api/notes', (req, res) => res.json(database));
